4976baf4bb99e37755b824d385e9a0da857283a5
tianat11 <tianat992@gmail.com>
2024-04-02 10:53:50 -0700

    added getting funcy folder and js2 files to week 1

--- Class Notes/Week 1/variable.js
+++ Class Notes/Week 1/variable.js
@@ -2,3 +2,13 @@ var myName="Andrea" //string
 var summer=false    //Boolean
 var canSwim         //undefined 
 var pets=null       //null
+
+let greeting = "Hello";
+console.log(greeting);
+const regularGreeting = greeting + '.';
+
+greeting = "Hi";
+
+let yelledGreeting = greeting + '!';
+console.log(yelledGreeting )
+let surprisedGreeting = "Oh! " + regularGreeting;
--- classwrk/script.js
+++ classwrk/script.js
@@ -1,1 +1,7 @@
-alert("hello, world!")
+let greeting = "Hello";
+const regularGreeting = greeting + '.';
+greeting = "Hi";
+let yelledGreeting = greeting + '!';
+let surprisedGreeting = "Oh! " + regularGreeting;
+
+console.log(surprisedGreeting);
