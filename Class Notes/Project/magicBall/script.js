de1015cc9f758aea04f54e398d788622fb416865
tianat11 <tianat992@gmail.com>
2024-06-05 11:00:34 -0700

    added magic ball project

---
+++ Class Notes/Project/magicBall/script.js
@@ -0,0 +1,21 @@
+// Step 1: Define Possible Answers
+
+
+
+// Step 2: Select HTML Elements
+
+
+
+//Step 3: Generate a Random Answer
+
+
+
+//Step 4: Rotate the Magic 8 Ball
+
+
+
+//Step 5: Display the Answer
+
+
+
+//Step 6: Add Event Listener to the Ball
