a7d2d02ae6e7094548e6f55aa5c03125ad16b92f
tianat11 <tianat992@gmail.com>
2024-06-10 10:34:05 -0700

    added week 10 assignments

---
+++ Class Notes/Week 10/Day 1/to-kingdom-come/main.js
@@ -0,0 +1,23 @@
+// 1. Function to append a DOM element to the Arguments <ul>
+function addStrikethroughToFirstArgument() {
+    const firstArgument = document.querySelector('#arguments li');
+    if (firstArgument) {
+        firstArgument.style.textDecoration = 'line-through';
+    }
+}
+
+addStrikethroughToFirstArgument();
+
+// 2. Create an image element and append it to the Arguments <ul>
+
+
+// 3. Function to change the height of an image element to 30 pixels
+
+// 4. Query an image element and change its height
+
+
+// 5. Function to assign the class `invisible` to an element
+
+
+// 6. Query an element and make it invisible
+
